a96f15c8d913c7626791c0a660780334ebc43bd9
Kevin Goes <kevin.goes@student.hu.nl>
2019-01-30 09:25:17 +0100

    Tests Update 4: Database connection test

--- Test_Database_Connection.py
+++ Test_Database_Connection.py
@@ -1,1 +1,15 @@
-niks
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='lpdstrtn' "
+                            "user='lpdstrtn' "
+                            "host='dumbo.db.elephantsql.com' "
+                            "password='OQBOtTdWyf5xbdDqwGFyi_T_rQ2KcjNw'")
+    c = conn.cursor()
+    c.execute("SELECT version();")
+    record = c.fetchone()
+    print("Connection established \n\n")
+    print("You are connected to -", record, "\n")
+
+except:
+    print("I am unable to connect to the database")
